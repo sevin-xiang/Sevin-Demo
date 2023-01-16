200683f352437f3a3928d6e1a7f434f959ad69a5
sevin-xiang <161250275@qq.com>
2023-01-16 08:58:23 +0800

    最终提交

--- partOne/src/TestMain.java
+++ partOne/src/TestMain.java
@@ -2,5 +2,6 @@ public class TestMain {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("2");
+        System.out.println("test change");
     }
 }
